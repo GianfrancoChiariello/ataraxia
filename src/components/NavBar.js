6f8dd3959b6dc06a2dbd13792359fb9d8a5a0dfc
Andromeda-h <gian.chiariello@gmail.com>
2022-09-21 18:53:14 -0300

    add

---
+++ src/components/NavBar.js
@@ -0,0 +1,36 @@
+    import CartWidget from "./CartWidget";
+
+    const NavBar = () => {
+    return (
+        <nav className="navbar navbar-dark navbar-expand-lg bg-dark">
+        <div className="container-fluid">
+            <label className="navbar-brand" >Ataraxia</label>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+                <li className="nav-item">
+                <a href="#" className="nav-link active" aria-current="page">Home</a>
+                </li>
+                <div className="btn-group">
+                <button type="button" className="btn text-white dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                    Productos
+                </button>
+                <ul className="dropdown-menu">
+                    <li><label className="dropdown-item">Cursos</label></li>
+                    <li><label className="dropdown-item">Sahumerios</label></li>
+                    <li><label className="dropdown-item">Perfumes</label></li>
+                </ul>
+                </div>
+            </ul>
+            </div>
+            <span className="navbar-text">
+            <CartWidget></CartWidget>
+            </span>
+        </div>
+        </nav>
+    );
+    }
+
+    export default NavBar;
